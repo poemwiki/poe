0b0520b30c7151ac6074cedf382ae9128839b5f4
Ray Victor <ray7551@gmail.com>
2020-08-06 11:45:40 +0800

    issue1 Wrong login validation message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,19 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     @include('layouts.icon')
+    @include('layouts.analyze')
     <title>{{config('app.name')}}</title>
-    <meta name="description" content="CoreUI Template - InfyOm Laravel Generator">
-    <meta name="keyword" content="CoreUI,Bootstrap,Admin,Template,InfyOm,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
-    <!-- Bootstrap-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@coreui/coreui@2.1.16/dist/css/coreui.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@icon/coreui-icons-free@1.0.1-alpha.1/coreui-icons-free.css">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css"
-          rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
+    <meta name="author" content="PoemWiki">
+    <meta name="description" content="PoemWiki">
+    <meta name="keyword" content="poem,poetry,poet,诗，诗歌，诗人">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@coreui/coreui@2.1.16/dist/css/coreui.min.css" integrity="sha256-lvsgjUQT72IM3r+HhRaDv7v2h6LS1707C13IAid9vwo=" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/simple-line-icons.min.css">
+
 </head>
 <body class="app flex-row align-items-center">
 <div class="container">
@@ -29,8 +25,8 @@
                     <div class="card-body">
                         <form method="post" action="{{ url('/login') }}">
                             @csrf
-                            <h1>Login</h1>
-                            <p class="text-muted">Sign In to your account</p>
+                            <h1>登录</h1>
+                            <p class="text-muted">登录到 PoemWiki</p>
                             <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">
@@ -51,7 +47,7 @@
                                       <i class="icon-lock"></i>
                                     </span>
                                 </div>
-                                <input type="password" class="form-control {{ $errors->has('password')?'is-invalid':'' }}" placeholder="Password" name="password">
+                                <input type="password" class="form-control {{ $errors->has('password')?'is-invalid':'' }}" placeholder="密码" name="password">
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
                                        <strong>{{ $errors->first('password') }}</strong>
@@ -60,23 +56,23 @@
                             </div>
                             <div class="row">
                                 <div class="col-6">
-                                    <button class="btn btn-primary px-4" type="submit">Login</button>
+                                    <button class="btn btn-primary px-4" type="submit">登录</button>
                                 </div>
                                 <div class="col-6 text-right">
                                     <a class="btn btn-link px-0" href="{{ url('/password/reset') }}">
-                                        Forgot password?
+                                        忘记密码
                                     </a>
                                 </div>
                             </div>
                         </form>
                     </div>
                 </div>
-                <div class="card text-white bg-primary py-5 d-md-down-none d-none" style="width:44%">
+                <div class="card text-white bg-primary py-5 d-md-down-none">
                     <div class="card-body text-center">
                         <div>
                             <h2>注册</h2>
-                            <p>暂时不开放注册。</p>
-                                <a class="btn btn-primary active mt-3" href="{{ url('/register') }}">Register Now!</a>
+                            <p>如果您还没有账号，<br>欢迎注册 PoemWiki！</p>
+                            <a class="btn btn-primary active mt-3" href="{{ url('/register') }}">注册</a>
                         </div>
                     </div>
                 </div>
@@ -84,11 +80,5 @@
         </div>
     </div>
 </div>
-<!-- CoreUI and necessary plugins-->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@2.1.16/dist/js/coreui.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/perfect-scrollbar.js"></script>
 </body>
 </html>
