08523052476796c7e5512d006e6015386d49b040
Ray Victor <ray7551@gmail.com>
2020-08-06 13:13:12 +0800

    cleaner layout & html structure

--- resources/views/layouts/fe.blade.php
+++ resources/views/layouts/fe.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="PoemWiki,@yield('author')">
+    <meta name="description" content="PoemWiki">
+    <meta name="keyword" content="poem,poetry,poet,诗,诗歌诗人,@yield('author'),@yield('title')">
     @include('layouts.icon')
     @include('layouts.analyze')
 
@@ -11,13 +14,12 @@
     <!-- Fonts -->
     <link href="{{ asset('css/post.css') }}" rel="stylesheet">
 </head>
-<body>
-<div class="position-ref">
-
-    <main class="flex-center">
+<body class="position-ref">
+    <div class="top-right links no-select">
+        <a class="site-name" href="{{$randomPoemUrl}}">POEM&#0010;Wiki</a>
+    </div>
+    <main class="post">
         @yield('content')
     </main>
-
-</div>
 </body>
 </html>
