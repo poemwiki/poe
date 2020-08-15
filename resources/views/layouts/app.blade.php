fe2a3a9163904d2ccd9323442535a3b5d5dd7c9a
Ray Victor <ray7551@gmail.com>
2020-08-15 17:05:29 +0800

    lowercase charset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <title>{{config('app.name')}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 @include('layouts.icon')
