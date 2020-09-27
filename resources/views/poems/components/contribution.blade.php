585b2292de766396ab37dfc9cec56024d602b0b5
Ray Victor <ray7551@gmail.com>
2020-09-28 01:32:54 +0800

    show shorter contribution list

---
+++ resources/views/poems/components/contribution.blade.php
@@ -0,0 +1,33 @@
+<ol id="contribution" class="contribution collapsed">
+    @foreach($logs as $key=>$log)
+        <li @if($key!==0 && $key!==count($logs)-1)
+            class="log-middle"
+            @endif><span title="{{$log->created_at}} UTC">{{\Illuminate\Support\Carbon::parse($log->created_at)->format('Y-m-d')}}</span> {{$log->causer_type === "App\User" ? \App\User::find($log->causer_id)->name : '系统'}} {{trans('poem.change type '.$log->description)}}
+            @php
+                $newVal = $log->properties->get('attributes');
+                $oldVal = $log->properties->get('old');
+                $props = array_keys($newVal);
+                //dd($props);
+            @endphp
+
+            @if($log->description === 'updated')
+                @foreach($props as $prop)
+                    @if($prop === 'poem')
+                        {{trans('admin.poem.columns.'.$prop)}}
+                    @elseif($prop === 'content_id')
+
+                    @elseif($prop === 'original_id')
+                        {{trans('poem.original poem')}}
+                    @else
+                        {{trans('admin.poem.columns.'.$prop)}} [ <del>{{$oldVal[$prop]}}</del> -> {{$newVal[$prop]}} ]
+                    @endif
+                @endforeach
+            @elseif($log->description === 'created')
+                @lang('poem.initial version')
+            @endif
+        </li>
+        @if($key === 0 && count($logs) > 2)
+            <a id="folder" class="btn">...</a>
+        @endif
+    @endforeach
+</ol><?php
--- resources/views/poems/show_fields.blade.php
+++ resources/views/poems/show_fields.blade.php
@@ -42,37 +42,12 @@ $createPageUrl = $poem->is_original ? route('poems/create', ['original_fake_id' 
         <a class="edit btn" href="{{ Auth::check() ? route('poems/edit', $fakeId) : route('login', ['ref' => route('poems/edit', $fakeId, false)]) }}">@lang('poem.correct errors or edit')</a>
         <a class="btn" href="{{ Auth::check() ? route('poems/create') : route('login', ['ref' => route('poems/create')]) }}">@lang('poem.add poem')</a>
         <ol id="contribution" class="contribution collapsed">
-            @foreach($logs as $key=>$log)
-                <li @if($key!==0 && $key!==count($logs)-1)
-                    class="log-middle"
-                    @endif><span title="{{$log->created_at}} UTC">{{\Illuminate\Support\Carbon::parse($log->created_at)->format('Y-m-d')}}</span> {{$log->causer_type === "App\User" ? \App\User::find($log->causer_id)->name : '系统'}} {{trans('poem.change type '.$log->description)}}
-                    @php
-                        $newVal = $log->properties->get('attributes');
-                        $oldVal = $log->properties->get('old');
-                        $props = array_keys($newVal);
-                        //dd($props);
-                    @endphp
-
-                    @if($log->description === 'updated')
-                        @foreach($props as $prop)
-                            @if($prop === 'poem')
-                                {{trans('admin.poem.columns.'.$prop)}}
-                            @elseif($prop === 'content_id')
-
-                            @elseif($prop === 'original_id')
-                                {{trans('poem.original poem')}}
-                            @else
-                                {{trans('admin.poem.columns.'.$prop)}} [ <del>{{$oldVal[$prop]}}</del> -> {{$newVal[$prop]}} ]
-                            @endif
-                        @endforeach
-                    @elseif($log->description === 'created')
-                        @lang('poem.initial version')
-                    @endif
-                </li>
-                @if($key === 0 && count($logs) > 2)
-                <a id="folder" class="btn">...</a>
-                @endif
-            @endforeach
+            @php
+            $latestLog = $logs[0];
+            $initialLog = $logs[count($logs) - 1];
+            @endphp
+            <li>@lang('poem.latest update') {{$latestLog->causer_type === "App\User" ? \App\User::find($latestLog->causer_id)->name : '系统'}}</li>
+            <li>@lang('poem.initial upload') {{$initialLog->causer_type === "App\User" ? \App\User::find($initialLog->causer_id)->name : '系统'}}</li>
         </ol>
 
         <dl class="poem-info">
