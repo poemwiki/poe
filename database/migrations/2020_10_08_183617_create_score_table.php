10551083a5dd137b4e644ac61e90356521f1f555
Ray Victor <ray7551@gmail.com>
2020-10-08 01:45:50 +0800

    score table

---
+++ database/migrations/2020_10_08_183617_create_score_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoreTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('score', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->unsignedBigInteger('poem_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedTinyInteger('score');
+            $table->unsignedBigInteger('content_id')->nullable();
+            $table->float('factor')->default(1);
+            $table->timestamps();
+            $table->index('poem_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('score');
+    }
+}
