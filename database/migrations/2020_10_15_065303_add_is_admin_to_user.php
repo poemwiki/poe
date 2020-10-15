975ddd2e2fc9fe59fae6e24dcd13edf414888e6c
Ray Victor <ray7551@gmail.com>
2020-10-16 01:39:19 +0800

    add is_admin, show writing month&date

--- database/migrations/2020_10_15_065303_add_is_admin_to_user.php
+++ database/migrations/2020_10_15_065303_add_is_admin_to_user.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIsAdminLastOnlineAtToUser extends Migration
+class AddIsAdminToUser extends Migration
 {
     /**
      * Run the migrations.
