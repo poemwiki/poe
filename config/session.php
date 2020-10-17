c9ff486cf156dc89599efa579bb52645eca241b3
Ray Victor <ray7551@gmail.com>
2020-10-18 03:34:02 +0800

    redis session

--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'file'),
+    'driver' => env('SESSION_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 60*240),
 
     'expire_on_close' => false,
 
