2b6dd56e41b49ac6120ebcb947dbda3b409d7344
Ray Victor <ray7551@gmail.com>
2020-11-03 23:53:15 +0800

    update keyword & description

--- resources/views/layouts/fe.blade.php
+++ resources/views/layouts/fe.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover">
     <meta name="author" content="PoemWiki,@yield('author')">
     <meta name="description" content="PoemWiki">
-    <meta name="keyword" content="@yield('title'),@yield('author'),poemwiki,poem,poetry,poet,诗,诗歌,诗人">
+
+    <meta name="keyword" content="@yield('title'),@yield('author'),@yield('title') 评论,@yield('title') 诗评,@yield('title') review,poemwiki,poem,poetry,poet,诗,诗歌,诗人,诗评,poem review">
+    <meta name="description" content="@yield('author') @yield('title') 诗歌全文,@yield('author') @yield('title') 评论、评分">
+
     @include('layouts.icon')
 
     <title>@yield('title') - {{config('app.name')}}</title>
