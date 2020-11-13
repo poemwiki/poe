87ba332649cf62b3037883f8a3a44a964fc39142
Ray Victor <ray7551@gmail.com>
2020-11-14 02:30:31 +0800

    add query route

--- routes/web.php
+++ routes/web.php
@@ -282,4 +282,6 @@ Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->gro
     });
 });
 
+
+Route::get('/q/{word}', 'QueryController@search')->name('search');
 Route::get('/{id}', 'PoemController@showPoem')->name('poem');
