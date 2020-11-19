08d7feff2a9ea62e774e56a0eeab18285e81b265
Ray Victor <ray7551@gmail.com>
2020-11-19 08:37:10 +0800

    bedtime:review {fromTimestamp?} {toTimestamp?}

--- app/Console/Bedtime/Review.php
+++ app/Console/Bedtime/Review.php
@@ -5,7 +5,11 @@ namespace App\Console\Bedtime;
 use App\Models\Review as ReviewModel;
 use App\Models\Score;
 use App\Models\WxPost;
+use EasyWeChat\Factory;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class Review extends Command {
     /**
@@ -13,7 +17,7 @@ class Review extends Command {
      *
      * @var string
      */
-    protected $signature = 'bedtime:review';
+    protected $signature = 'bedtime:review {fromTimestamp?} {toTimestamp?}';
 
     /**
      * The console command description.
@@ -38,35 +42,59 @@ class Review extends Command {
      */
     public function handle() {
         // $this->addBedtimeScore(3054, 44);
-        $this->addBedtimeReview(2651625905, 44);
+        $this->addBedtimeReview(
+            $this->argument('fromTimestamp') ?? Date::createFromDate(2020, 8, 21)->getTimestamp(),
+            $this->argument('toTimestamp') ?? Date::createFromDate(2020, 12, 15)->getTimestamp()
+        );
         return 0;
     }
 
 
-    public function addBedtimeReview($maxId = 0, $userId = 44) {
-        WxPost::where('appmsgid', '<=', $maxId)->whereNotNull('poem_id')
-            ->get()->each(function ($post) use ($userId) {
-            ReviewModel::updateOrInsert([
-                'poem_id' => $post->poem_id,
-                'user_id' => $userId
-            ], [
-                'poem_id' => $post->poem_id,
-                'user_id' => $userId,
-                'content' => <<<blade
-我在《{$post->title}》&nbsp;&nbsp; <a href="{$post->link}" target="_blank">{$post->link}</a> &nbsp;&nbsp;这篇公众号文章里提到了这首诗
+    public function addBedtimeReview($minCreateTime = 0, $maxCreateTime = 0, $userId = 44) {
+        $config = [
+            'app_id' => env('WECHAT_OFFICIAL_ACCOUNT_APPID'),
+            'secret' => env('WECHAT_OFFICIAL_ACCOUNT_SECRET'),
+
+            // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+            'response_type' => 'object',
+        ];
+
+        $app = Factory::officialAccount($config);
+
+        WxPost::whereBetween('create_time', [$minCreateTime, $maxCreateTime])->whereNotNull('poem_id')
+            ->get()->each(function ($post) use ($userId, $app) {
+
+                $shortUrl = $app->url->shorten($post->link);
+                if($shortUrl->errcode === 0) {
+                    $link = $shortUrl->short_url;
+                } else {
+                    $link = Str::of($post->link)->replaceMatches('@&chksm=[^#&]*@', '')
+                        ->replace('#rd', '')
+                        ->replace('#wechat_redirect', '');
+                }
+                Log::info('Add bedtimepoem review link: ' . $link);
+
+                ReviewModel::updateOrInsert([
+                    'poem_id' => $post->poem_id,
+                    'user_id' => $userId
+                ], [
+                    'poem_id' => $post->poem_id,
+                    'user_id' => $userId,
+                    'content' => <<<blade
+我在《{$post->title}》&nbsp;&nbsp; <a href="{$link}" target="_blank">{$link}</a> &nbsp;&nbsp;这篇公众号文章里提到了这首诗
 blade,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        });
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            });
     }
 
     public function addBedtimeScore($maxId = 3054, $userId = 44) {
         $exceptIds = [514, 515, 516, 517, 518];
         $data = [];
         for ($id = 1; $id <= $maxId; $id++) {
-            if(in_array($id, $exceptIds)) continue;
-            $data[] = ['poem_id' => $id, 'user_id' => $userId, 'created_at' => now(), 'updated_at' => now(), 'score' => 4, 'weight' => 1.0];
+            if (in_array($id, $exceptIds)) continue;
+            $data[] = ['poem_id' => $id, 'user_id' => $userId, 'created_at' => now(), 'updated_at' => now(), 'score' => 5, 'weight' => 1.0];
         }
         Score::updateOrInsert([
             'poem_id' => $id,
