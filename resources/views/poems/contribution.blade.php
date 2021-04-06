9b755991489c311c0d84d6872d733f353d294a42
Ray Victor <ray7551@gmail.com>
2021-04-06 13:13:22 +0800

    fix contribution show error

--- resources/views/poems/contribution.blade.php
+++ resources/views/poems/contribution.blade.php
@@ -47,7 +47,7 @@
         </li>
     @endforeach
 
-    @if(count($poem->activityLogs)<1)
+    @if(count($poem->activityLogs)<1 or $poem->activityLogs->last()->description !== 'created')
         <li title="{{$poem->created_at}}"><span class="field">@lang('poem.initial upload')</span> PoemWiki</li>
     @endif
 </ol>
