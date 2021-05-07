725dc997f5c43f4834b9c0d81768f9aaeb67d61e
Ray Victor <ray7551@gmail.com>
2021-05-08 01:22:41 +0800

    add contribution view for is_original, original_id

--- resources/views/poems/contribution.blade.php
+++ resources/views/poems/contribution.blade.php
@@ -62,14 +62,23 @@ use Jfcherng\Diff\Factory\RendererFactory;
 
                       echo $result;
                   @endphp
-                @elseif($prop === 'poet_id')
+                  @elseif($prop === 'poet_id')
                     @php
                       $oldAuthor = $old ? App\Models\Author::find($old) : null;
                       $newAuthor = $new ? App\Models\Author::find($new) : null;
                     @endphp
                     <span class="field">{{trans('admin.poem.columns.poet_id')}}</span>&nbsp;[&nbsp;
                     <del>@if($oldAuthor) <a href="{{$oldAuthor->url}}">{{$oldAuthor->label}}</a> @endif</del>&nbsp;⟹&nbsp;
-                      @if($newAuthor) <a href="{{$newAuthor->url}}">{{$newAuthor->label}}</a> @endif
+                    @if($newAuthor) <a href="{{$newAuthor->url}}">{{$newAuthor->label}}</a> @endif
+                    &nbsp;]
+                  @elseif($prop === 'translator_id')
+                    @php
+                      $oldTranslator = $old ? App\Models\Author::find($old) : null;
+                      $newTranslator = $new ? App\Models\Author::find($new) : null;
+                    @endphp
+                    <span class="field">{{trans('admin.poem.columns.translator_id')}}</span>&nbsp;[&nbsp;
+                    <del>@if($oldTranslator) <a href="{{$oldTranslator->url}}">{{$oldTranslator->label}}</a> @endif</del>&nbsp;⟹&nbsp;
+                    @if($newTranslator) <a href="{{$newTranslator->url}}">{{$newTranslator->label}}</a> @endif
                     &nbsp;]
                 @elseif($prop === 'content_id')
                     @php
@@ -79,12 +88,20 @@ use Jfcherng\Diff\Factory\RendererFactory;
                       // <span class="field">{{trans('admin.poem.columns.content_id')}}</span>&nbsp;[&nbsp;<del>{{$oldContent ? $oldContent->content : ''}}</del>&nbsp;⟹&nbsp;{{$newContent ? $newContent->content : ''}}&nbsp;]
                     @endphp
                     <span class="field">{{trans('admin.poem.columns.content_id')}}</span>&nbsp;[&nbsp;<del>{{$old}}</del>&nbsp;⟹&nbsp;{{$new}}&nbsp;]
+                @elseif($prop === 'is_original')
+                  <span class="field">{{trans('admin.poem.columns.is_original')}}</span>&nbsp;[&nbsp;<del>{{trans_choice('admin.poem.is_original', $old)}}</del>&nbsp;⟹&nbsp;{{trans_choice('admin.poem.is_original', $new)}}&nbsp;]
                 @elseif($prop === 'language_id')
                   <span class="field">{{trans('admin.poem.columns.language_id')}}</span>&nbsp;[&nbsp;<del>{{$old ? $languageList[$old]->name_lang : ''}}</del>&nbsp;⟹&nbsp;{{$new ? $languageList[$new]->name_lang : ''}}&nbsp;]
                 @elseif($prop === 'genre_id')
                   <span class="field">{{trans('admin.poem.columns.genre_id')}}</span>&nbsp;[&nbsp;<del>{{$old ? $genreList[$old]->name_lang : ''}}</del>&nbsp;⟹&nbsp;{{$new ? $genreList[$new]->name_lang : ''}}&nbsp;]
                 @elseif($prop === 'original_id')
-                    <span class="field">{{trans('poem.original poem')}}</span>
+                    @php
+                      $oldPoem = $old ? App\Models\Poem::find($old) : null;
+                      $newPoem = $new ? App\Models\Poem::find($new) : null;
+                      $oldLink = $oldPoem ? "<a href=\"$oldPoem->url\">$oldPoem->title</a>" : '';
+                      $newLink = $newPoem ? "<a href=\"$newPoem->url\">$newPoem->title</a>" : '';
+                    @endphp
+                    <span class="field">{{trans('poem.original work')}}</span>&nbsp;[&nbsp;<del>{!!$oldLink!!}</del>&nbsp;⟹&nbsp;{!!$newLink!!}&nbsp;]
                 @else
                     <span class="field">{{trans('admin.poem.columns.'.$prop)}}</span>&nbsp;[&nbsp;<del>{{$old}}</del>&nbsp;⟹&nbsp;{{$new}}&nbsp;]
                 @endif
