582ae72317dc022a295f6f633f70d2c767b926ae
Ray Victor <ray7551@gmail.com>
2021-05-07 16:13:26 +0800

    remove clipboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
-//import ElementUI from 'element-ui';
-//import { Message } from 'element-ui';
-import clipboard from 'clipboard';
+
 require('./bootstrap');
 
 
@@ -12,32 +10,6 @@ require('./bootstrap');
 new Vue({
   el: '#app',
   mounted() {
-    let board = new clipboard('#copy');
-    let delay = 1000;
-    let $copy = $('#copy');
-    board.on('success', function(e) {
-      $copy.tooltip({
-        title: '复制成功',
-        trigger: 'click'
-      });
-      $copy.tooltip('show');
-      window.setTimeout(() => {
-        $copy.tooltip('dispose');
-      }, delay);
-
-      e.clearSelection();
-    });
-
-    board.on('error', function(e) {
-      $copy.tooltip({
-        title: "请复制以下链接：\n" + e.text,
-        trigger: 'click'
-      });
-      $copy.tooltip('show');
-      window.setTimeout(() => {
-        $copy.tooltip('hide');
-      }, delay);
-    });
   },
 
   methods: {
