b8f049021d75a47169aa4bea78f3025b8dc06279
Ray Victor <ray7551@gmail.com>
2021-09-03 23:30:53 +0800

    merge poem

--- resources/js/admin/app-components/Listing/AppListing.js
+++ resources/js/admin/app-components/Listing/AppListing.js
@@ -1,5 +1,44 @@
 import { BaseListing } from 'craftable';
 
 export default {
-	mixins: [BaseListing]
+	mixins: [BaseListing],
+  methods: {
+	  mergeToItem: function(url) {
+      this.$modal.show('dialog', {
+        title: '请谨慎操作！',
+        text: '请填入合并到的诗歌 ID<input id="merge-to-id" type="number" />',
+        buttons: [{ title: '取消' }, {
+          title: '<span class="btn-dialog btn-danger">确认合并<span>',
+          handler: () => {
+            this.$modal.hide('dialog');
+            axios.post(url + '/merge/' + $('#merge-to-id').val()).then((response) => {
+              this.loadData();
+              this.$notify({ type: 'success', title: 'Success!', text: response.data.message ? response.data.message : '删除合并。' });
+            }, (error) => {
+              this.$notify({ type: 'error', title: 'Error!', text: error.response.data.message ? error.response.data.message : '出错啦😭' });
+            });
+          }
+        }]
+      });
+    },
+
+    deleteItem: function deleteItem(url) {
+      this.$modal.show('dialog', {
+        title: '请谨慎操作！',
+        text: '确实要删除此项？',
+        buttons: [{ title: '取消' }, {
+          title: '<span class="btn-dialog btn-danger">确认删除<span>',
+          handler: () => {
+            this.$modal.hide('dialog');
+            axios.delete(url).then((response) => {
+              this.loadData();
+              this.$notify({ type: 'success', title: 'Success!', text: response.data.message ? response.data.message : '删除成功。' });
+            }, (error) => {
+              this.$notify({ type: 'error', title: 'Error!', text: error.response.data.message ? error.response.data.message : '出错啦😭' });
+            });
+          }
+        }]
+      });
+    },
+  }
 };
