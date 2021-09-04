9a88d56b7a4950e65d97b65223e5640c80d87bb9
Ray Victor <ray7551@gmail.com>
2021-09-05 00:38:42 +0800

    add 2 error code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,33 +8,38 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
 
     public static $CODE = [
-        'success' => 0,
-        'general_error' => -1,
+        'success'                 => 0,
+        'general_error'           => -1,
         'content_security_failed' => -2,
-        'no_entry' => -3,
-        'duplicated' => -4,
+        'no_entry'                => -3,
+        'duplicated'              => -4,
+        'invalid_poem_length'     => -5,
+        'poem_content_invalid'    => -6
     ];
 
     public function response($data, string $message = '', int $code = 0) {
-        if(is_string($data)) {
+        if (is_string($data)) {
             return [
                 'redirect' => $data,
-                'message' => $message,
-                'code' => $code
+                'message'  => $message,
+                'code'     => $code
             ];
         }
+
         return compact('data', 'message', 'code');
     }
 
-    public function responseSuccess($data=[], $message = null) {
+    public function responseSuccess($data = [], $message = null) {
         return $this->response(
             $data, $message ?? '');
     }
 
-    public function responseFail($data=[], $message = null, int $code = -1) {
+    public function responseFail($data = [], $message = null, int $code = -1) {
         return $this->response(
             $data, $message ?? trans('fail'), $code);
     }
