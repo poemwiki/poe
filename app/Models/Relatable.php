4b3c1ec1cffa397b2a9d0e05d4bf9f098c4a52ee
Ray Victor <ray7551@gmail.com>
2021-09-05 00:46:49 +0800

    merge to main poem

--- app/Models/Relatable.php
+++ app/Models/Relatable.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Support\Collection;
 use Spatie\Activitylog\Models\Activity;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 /*
  * This model will be used to log activity.
@@ -15,44 +15,60 @@ use Spatie\Activitylog\Models\Activity;
  */
 
 /**
- * App\Models\Relation
+ * App\Models\Relation.
  *
- * @property int $id
- * @property int relation
- * @property string|null $start_type
- * @property int|null $start_id
- * @property string|null $end_type
- * @property int|null $end_id
- * @property Collection|null $properties
+ * @property int                             $id
+ * @property int                             $relation
+ * @property string|null                     $start_type
+ * @property int|null                        $start_id
+ * @property string|null                     $end_type
+ * @property int|null                        $end_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @mixin \Eloquent
  */
 class Relatable extends MorphPivot {
-    const NODE_TYPE = [
-        'poem' => \App\Models\Poem::class,
+    use LogsActivity;
+    public $incrementing = true; // to avoid activity log subject_id null. see https://github.com/spatie/laravel-activitylog/issues/598#issuecomment-535009005
+
+    protected static $logFillable          = true;
+    protected static $logOnlyDirty         = true;
+    public static $ignoreChangedAttributes = ['created_at', 'updated_at'];
+
+    public const NODE_TYPE = [
+        'poem'   => \App\Models\Poem::class,
         'author' => \App\Models\Author::class
     ];
-    const RELATION = [
-        'poet_is' => 0,
-        'translator_is' => 1,
-        'merged_to_poem' => 2,
+    public const RELATION = [
+        'poet_is'          => 0,
+        'translator_is'    => 1,
+        'merged_to_poem'   => 2,
         'merged_to_author' => 3,
-        'heteronymy_of' => 4
+        'heteronymy_of'    => 4,
+        'has_egg'          => 5
     ];
     // TODO check for start_type and end_type before add relation
     // TODO check for relation limit before add relation(e.g. poem1 has been merged to poem2, then poem1 can not be merged to poem3)
-    const RELATION_RULES = [
-        'poet_is' => ['start_type' => Poem::class, 'end_type' => Author::class, 'limit' => 1],
-        'translator_is' => ['start_type' => Poem::class, 'end_type' => Author::class, 'limit' => 6],
-        'merged_to_poem' => ['start_type' => Poem::class, 'end_type' => Poem::class, 'limit' => 1],
-        'merged_to_author' => ['start_type' => Author::class, 'end_type' => Author::class, 'limit' => 1]
+    public const RELATION_RULES = [
+        'poet_is'          => ['start_type' => Poem::class, 'end_type' => Author::class, 'limit' => 1],
+        'translator_is'    => ['start_type' => Poem::class, 'end_type' => Author::class, 'limit' => 6],
+        'merged_to_poem'   => ['start_type' => Poem::class, 'end_type' => Poem::class, 'limit' => 1],
+        'merged_to_author' => ['start_type' => Author::class, 'end_type' => Author::class, 'limit' => 1],
+        'has_egg'          => ['start_type' => Poem::class, 'end_type' => Egg::class, 'limit' => 1]
     ];
 
-    const CREATED_AT = 'created_at';
-    const UPDATED_AT = 'updated_at';
+    public const CREATED_AT = 'created_at';
+    public const UPDATED_AT = 'updated_at';
 
-    protected $table = 'relatable';
+    protected $table    = 'relatable';
+    protected $fillable = [
+        'start_type',
+        'start_id',
+        'end_type',
+        'end_id',
+        'relation',
+        'properties'
+    ];
     protected $dates = [
         'updated_at',
         'created_at'
@@ -60,34 +76,35 @@ class Relatable extends MorphPivot {
 
     // protected $appends = ['changes', 'logs'];
 
-    public function start() : MorphTo {
+    public function start(): MorphTo {
         return $this->morphTo();
     }
-    public function end() : MorphTo {
+
+    public function end(): MorphTo {
         return $this->morphTo();
     }
 
     public function scopeTranslatorIs($query, $startType, $startId) {
         $query->where([
-            'relation' => Relatable::RELATION['translator_is'],
+            'relation'   => Relatable::RELATION['translator_is'],
             'start_type' => $startType,
-            'start_id' => $startId
+            'start_id'   => $startId
         ]);
     }
 
     public function scopePoetIs($query, $startType, $startId) {
         $query->where([
-            'relation' => Relatable::RELATION['poet_is'],
+            'relation'   => Relatable::RELATION['poet_is'],
             'start_type' => $startType,
-            'start_id' => $startId
+            'start_id'   => $startId
         ]);
     }
 
-    public function scopeMergedTo($query, $startType, $startId) {
+    public function scopeMergedToPoem($query, $startType, $startId) {
         $query->where([
-            'relation' => Relatable::RELATION['merged_to_poem'],
+            'relation'   => Relatable::RELATION['merged_to_poem'],
             'start_type' => $startType,
-            'start_id' => $startId
+            'start_id'   => $startId
         ])->limit(1);
     }
 }
