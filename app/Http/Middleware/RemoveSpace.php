b05b6603912c6e8fb7a176d9f40d8af9a251c0ca
Ray Victor <ray7551@gmail.com>
2021-10-03 00:56:53 +0800

    disable removeSpace for contribution page

--- app/Http/Middleware/RemoveSpace.php
+++ app/Http/Middleware/RemoveSpace.php
@@ -1,31 +1,30 @@
 <?php
 
-
 namespace App\Http\Middleware;
-
 
 use RenatoMarinho\LaravelPageSpeed\Middleware\PageSpeed;
 
-class RemoveSpace extends PageSpeed{
+class RemoveSpace extends PageSpeed {
     // TODO replace spaces for html of livewire response
     public function apply($buffer) {
         $replace = [
-//            "/\n([\S])/" => '$1',
-//            "/\r/" => '',
-//            "/\n/" => '',
-//            "/\t/" => '',
-//            "/ +/" => ' ',
-//             "@(/\w+>\s*[^\s]*)\s*<@" => '$1<',
+            //            "/\n([\S])/" => '$1',
+            //            "/\r/" => '',
+            //            "/\n/" => '',
+            //            "/\t/" => '',
+            //            "/ +/" => ' ',
+            //             "@(/\w+>\s*[^\s]*)\s*<@" => '$1<',
             // remove  spaces around block elements tag
             "@\s*(</?(head|meta|title|link|address|article|h1|h2|h3|h4|h5|h6|aside|blockquote|details|dialog|dd|div|dl|dt|fieldset|figcaption|figure|footer|form|h1|header|hgroup|hr|li|main|nav|ol|p|section|table|ul)(\s+[^>]+)*>)\s*@" => '$1',
             // remove spaces around inline elements tag
             "@\s*(</?(a|abbr|acronym|audio|b|bdi|bdo|big|br|button|canvas|cite|data|datalist|dfn|em|embed|i|iframe|img|input|kbd|label|map|mark|meter|noscript|object|output|picture|progress|q|ruby|s|samp|script|select|slot|small|span|strong|sub|sup|svg|template|textarea|time|u|tt|var|video|wbr)(\s+[^>]+)*>)\s*@" => '$1',
             // remove spaces before pre/code tag
-            "@\s*(<(pre|code|del|ins)(\s+[^>]+)*>)@" => '$1',
+            "@\s*(<(pre|code)(\s+[^>]+)*>)@" => '$1',
         ];
 
         $str = $this->replace($replace, $buffer);
-        return preg_replace_callback('@<\w+[^>]*>@', function ($matches){
+
+        return preg_replace_callback('@<\w+[^>]*>@', function ($matches) {
             // replace spaces between attrs
             return preg_replace('@([\w\-:]+(?:="[^"]*")?)\s+@', '$1 ', $matches[0]);
         }, $str);
