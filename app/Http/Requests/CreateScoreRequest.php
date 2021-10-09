b5fef148eb9333840de473cf49a3239bc7d2cacf
Ray Victor <ray7551@gmail.com>
2021-10-09 20:15:22 +0800

    update score policy

--- app/Http/Requests/CreateScoreRequest.php
+++ app/Http/Requests/CreateScoreRequest.php
@@ -4,17 +4,13 @@ namespace App\Http\Requests;
 
 use App\Models\Poem;
 use App\Models\Score;
-use App\Repositories\AuthorRepository;
-use App\Repositories\LanguageRepository;
 use App\User;
 use Illuminate\Foundation\Http\FormRequest;
-use App\Models\Review;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
 class CreateScoreRequest extends FormRequest {
-
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -31,20 +27,20 @@ class CreateScoreRequest extends FormRequest {
      */
     public function rules() {
         return [
-            'score' => ['required', 'integer', Rule::in(Score::$SCORE)], // TODO it should be named as "rating"
+            'score'   => ['required', 'integer', Rule::in(Score::$SCORE)], // TODO it should be named as "rating"
             'poem_id' => ['required', 'integer', 'exists:' . \App\Models\Poem::class . ',id'],
         ];
     }
 
     /**
-     * Modify input data
+     * Modify input data.
      *
      * @return array
      */
     public function getSanitized(): array {
         $sanitized = $this->validated();
 
-        $user = Auth::user();
+        $user                 = Auth::user();
         $sanitized['user_id'] = $user->id;
 
         $poem = Poem::find($sanitized['poem_id']);
@@ -56,22 +52,22 @@ class CreateScoreRequest extends FormRequest {
 
     public static function getScoreWeight(Poem $poem, User $user) {
         // TODO weight should from user.weight
-        if($poem->is_owner_uploaded===Poem::$OWNER['uploader'] && $poem->uploader) {
+        if ($poem->is_owner_uploaded === Poem::$OWNER['uploader'] && $poem->uploader) {
             if ($poem->uploader->id === $user->id) {
-                return 1;
+                return 0;
             }
         }
         // TODO $poem->is_owner_uploaded===Poem::$OWNER['translatorUploader']
-        if($poem->poetAuthor && $poem->poetAuthor->user && $poem->poetAuthor->user->id === $user->id) {
-            return 1;
+        if ($poem->poetAuthor && $poem->poetAuthor->user && $poem->poetAuthor->user->id === $user->id) {
+            return 0;
         }
 
         $isMaster = false;
-        $tags = $poem->tags;
-        if($tags->count() && $tags[0] && $tags[0]->campaign) {
+        $tags     = $poem->tags;
+        if ($tags->count() && $tags[0] && $tags[0]->campaign) {
             /** @var \App\Models\Campaign $campaign */
             $campaign = $tags[0]->campaign;
-            $isMaster = $campaign->isMaster($user->id) || $user->id===29;
+            $isMaster = $campaign->isMaster($user->id) || $user->id === 29;
         }
 
         // TODO $poem->translators has user
