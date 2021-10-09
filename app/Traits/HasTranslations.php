6f4a14e6e3684fa1ad09de4aefaf3a24e92aecff
Ray Victor <ray7551@gmail.com>
2021-10-10 04:16:01 +0800

    ugly fix of timezone issue

--- app/Traits/HasTranslations.php
+++ app/Traits/HasTranslations.php
@@ -6,11 +6,9 @@ use Brackets\Translatable\Traits\HasTranslations as ParentHasTranslations;
 use Illuminate\Support\Str;
 
 trait HasTranslations {
-
     use ParentHasTranslations;
 
     protected $locale;
-
 
     /**
      * Get an attribute from the model.
@@ -27,17 +25,37 @@ trait HasTranslations {
     }
 
     /**
+     * Convert the model instance to an array.
+     *
+     * By default, translations of only current locale of the model of each translated attribute is returned
+     *
+     * @return array
+     */
+    public function toArray(): array {
+        $array = parent::toArray();
+        collect($this->getTranslatableAttributes())->map(function ($attribute) use (&$array) {
+            $array[$attribute] = $this->getAttributeValue($attribute);
+        });
+
+        date_default_timezone_set(config('app.timezone'));
+
+        return $array;
+    }
+
+    /**
      * @param string $key
      * @param string $locale
      * @return mixed|string
      */
     public function fallback(string $key, string $locale) {
         $translations = parent::getAttributeValue($key);
-        if(empty($translations)) return '';
+        if (empty($translations)) {
+            return '';
+        }
 
         $lastFallback = array_key_first($translations);
-        $zhFallback = ['zh-hans', 'zh-cn', 'zh-Hans-CN', 'zh', 'zh-yue', 'zh-hant', 'zh-hk', 'zh-tw', 'zh-sg', 'wuu', 'yue', 'en', strtolower($lastFallback)];
-        $zhTFallback = ['zh-hant', 'zh-hk', 'zh-tw', 'zh', 'zh-cn', 'zh-hans', 'zh-yue', 'zh-Hans-CN', 'zh-sg', 'wuu', 'yue', 'en', strtolower($lastFallback)];
+        $zhFallback   = ['zh-hans', 'zh-cn', 'zh-Hans-CN', 'zh', 'zh-yue', 'zh-hant', 'zh-hk', 'zh-tw', 'zh-sg', 'wuu', 'yue', 'en', strtolower($lastFallback)];
+        $zhTFallback  = ['zh-hant', 'zh-hk', 'zh-tw', 'zh', 'zh-cn', 'zh-hans', 'zh-yue', 'zh-Hans-CN', 'zh-sg', 'wuu', 'yue', 'en', strtolower($lastFallback)];
         // TODO in_array($lowerLocale, $zhCNLocales)
         $lowerLocale = strtolower($locale);
         if (in_array($lowerLocale, $zhFallback)) {
@@ -54,39 +72,39 @@ trait HasTranslations {
     public function getTranslated(string $key, string $locale) {
         $translation = $this->getTranslation($key, $locale, false);
 
-        if(!empty($translation)) {
+        if (!empty($translation)) {
             return $translation;
         }
 
         $lowerLocale = strtolower($locale);
 
         $translation = $this->getTranslation($key, $lowerLocale, false);
-        if(!empty($translation)) {
+        if (!empty($translation)) {
             return $translation;
         }
-
 
         return $this->fallback($key, $locale);
     }
 
-    function getFallbackTranslation(string $key, array $fallbackArr): string {
+    public function getFallbackTranslation(string $key, array $fallbackArr): string {
         foreach ($fallbackArr as $locale) {
             $translation = $this->getTranslation($key, $locale, false);
-            if(!empty($translation)) {
+            if (!empty($translation)) {
                 // logic below is for wikidata.description_lang who's $translation is an array indexed by locales
-                if(is_array($translation)) {
-                    if(isset($translation[$locale])) {
+                if (is_array($translation)) {
+                    if (isset($translation[$locale])) {
                         return $translation[$locale];
                     }
+
                     continue;
                 }
 
                 return $translation;
             }
         }
+
         return '';
     }
-
 
     public function setAttribute($key, $value) {
         if ($this->isTranslatableAttribute($key) && is_array($value)) {
@@ -99,7 +117,7 @@ trait HasTranslations {
         }
 
         // Pass arrays and untranslatable attributes to the parent method.
-        if (! $this->isTranslatableAttribute($key) || is_array($value)) {
+        if (!$this->isTranslatableAttribute($key) || is_array($value)) {
             return parent::setAttribute($key, $value);
         }
 
@@ -107,6 +125,4 @@ trait HasTranslations {
         // translation for the current app locale.
         return $this->setTranslation($key, $this->getLocale(), $value);
     }
-
-
 }
