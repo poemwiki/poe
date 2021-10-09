f7c0d0f449e05dfe7d76de5173f96da16bcd9578
Ray Victor <ray7551@gmail.com>
2021-10-10 04:15:15 +0800

    update score policy

--- app/Http/Requests/CreateScoreRequest.php
+++ app/Http/Requests/CreateScoreRequest.php
@@ -45,12 +45,13 @@ class CreateScoreRequest extends FormRequest {
 
         $poem = Poem::find($sanitized['poem_id']);
 
-        $sanitized['weight'] = self::getScoreWeight($poem, $user);
+        $sanitized['weight'] = self::getScoreWeight($poem, $user, $sanitized['score']);
 
         return $sanitized;
     }
 
-    public static function getScoreWeight(Poem $poem, User $user) {
+    public static function getScoreWeight(Poem $poem, User $user, int $score) {
+        $lowScore = 4;
         // TODO weight should from user.weight
         if ($poem->is_owner_uploaded === Poem::$OWNER['uploader'] && $poem->uploader) {
             if ($poem->uploader->id === $user->id) {
@@ -68,6 +69,20 @@ class CreateScoreRequest extends FormRequest {
             /** @var \App\Models\Campaign $campaign */
             $campaign = $tags[0]->campaign;
             $isMaster = $campaign->isMaster($user->id) || $user->id === 29;
+        }
+
+        if (!$isMaster) {
+            $recentScore = Score::where([
+                ['user_id', '=', $user->id],
+                ['updated_at', '>=', now()->addMinutes(-1)],
+                ['score', '<=', $lowScore]
+            ])->get();
+            $count = $recentScore->count();
+            if ($count >= 2 && $score <= $lowScore) {
+                logger()->info('Score frequency limit touched by user', ['id' => $user->id, 'name' => $user->name]);
+
+                return 0;
+            }
         }
 
         // TODO $poem->translators has user
