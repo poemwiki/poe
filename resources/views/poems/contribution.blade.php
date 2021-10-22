13fbe1803df108f6962023f43439f3b709b0a5e2
Ray Victor <ray7551@gmail.com>
2021-10-23 02:14:18 +0800

    move contribution log to different files

--- resources/views/poems/contribution.blade.php
+++ resources/views/poems/contribution.blade.php
@@ -1,8 +1,4 @@
-<?php
-use Jfcherng\Diff\Differ;
-use Jfcherng\Diff\DiffHelper;
-use Jfcherng\Diff\Factory\RendererFactory;
-?>@extends('layouts.fe')
+@extends('layouts.fe')
 
 
 @section('title'){{$poem->title}}@endsection
@@ -38,113 +34,8 @@ use Jfcherng\Diff\Factory\RendererFactory;
                     $new = $newVal[$prop] ?? '';
                 @endphp
 
-            <div class="log">
-                @if($prop === 'poem')
-                    <span class="field">{{trans('admin.poem.columns.'.$prop)}}</span>
-                  @php
-                      $differOptions = [
-                          // show how many neighbor lines
-                          // Differ::CONTEXT_ALL can be used to show the whole file
-                          'context' => 1,
-                          // ignore case difference
-                          'ignoreCase' => false,
-                          // ignore whitespace difference
-                          'ignoreWhitespace' => false,
-                      ];
-                      $rendererOptions = [
-                          'detailLevel' => 'char', // (none, line, word, char)
-                          'language' => 'chs',
-                          'separateBlock' => true, // show a separator between different diff hunks in HTML renderers
-                          'spacesToNbsp' => true
-                      ];
+                @include('poems.log.log')
 
-                      $differ = new Differ(explode("\n", $old), explode("\n", $new), $differOptions);
-                      $renderer = RendererFactory::make('SideBySide', $rendererOptions); // or your own renderer object
-                      //dd($renderer, $old, $new);
-                      $result = $renderer->render($differ);
-
-                      echo $result;
-                  @endphp
-                @elseif($prop === 'poet_id')
-                    @php
-                      $oldAuthor = $old ? App\Models\Author::find($old) : null;
-                      $newAuthor = $new ? App\Models\Author::find($new) : null;
-                    @endphp
-                    <span class="field">{{trans('admin.poem.columns.poet_id')}}</span>&nbsp;[&nbsp;
-                    <del>@if($oldAuthor) <a href="{{$oldAuthor->url}}">{{$oldAuthor->label}}</a> @endif</del>&nbsp;⟹&nbsp;
-                    @if($newAuthor) <a href="{{$newAuthor->url}}">{{$newAuthor->label}}</a> @endif
-                    &nbsp;]
-                @elseif($prop === 'translator_id')
-                    @php
-                      $oldTranslator = $old ? App\Models\Author::find($old) : null;
-                      $newTranslator = $new ? App\Models\Author::find($new) : null;
-                    @endphp
-                    <span class="field">{{trans('admin.poem.columns.translator_id')}}</span>&nbsp;[&nbsp;
-                    <del>@if($oldTranslator) <a href="{{$oldTranslator->url}}">{{$oldTranslator->label}}</a> @endif</del>&nbsp;⟹&nbsp;
-                    @if($newTranslator) <a href="{{$newTranslator->url}}">{{$newTranslator->label}}</a> @endif
-                    &nbsp;]
-                @elseif($prop === 'translator')
-                    @php
-                      $oldTrans = json_decode($old);
-                      $oldTrans = is_array($oldTrans) ? $oldTrans : [$old];
-
-                      $newTrans = json_decode($new);
-                      $newTrans = is_array($newTrans) ? $newTrans : [$new];
-                    @endphp
-                    <span class="field">{{trans('admin.poem.columns.translator')}}</span>&nbsp;[&nbsp;
-                    <del>@foreach($oldTrans as $k => $t)
-                           @if(is_string($t))
-                             {{$t}}
-                           @elseif(is_numeric($t))
-                             @php
-                             $transAuthor = \App\Models\Author::find($t);
-                             @endphp
-                             @if($transAuthor)
-                              <a href="{{$transAuthor->url}}">{{$transAuthor->label}}</a>
-                             @endif
-                           @endif
-                           @if($k < count($oldTrans)-1),&nbsp;@endif
-                      @endforeach</del>&nbsp;⟹&nbsp;
-                    @foreach($newTrans as $i => $t)
-                      @if(is_string($t))
-                        {{$t}}
-                      @elseif(is_numeric($t))
-                        @php
-                          $transAuthor = \App\Models\Author::find($t);
-                        @endphp
-                        @if($transAuthor)
-                          <a href="{{$transAuthor->url}}">{{$transAuthor->label}}</a>
-                        @endif
-                      @endif
-                      @if($i < count($newTrans)-1),&nbsp;@endif
-                    @endforeach
-                    &nbsp;]
-                @elseif($prop === 'content_id')
-                    @php
-                      // TODO Why poem.content_id not stored into log?
-                      $oldContent = $old ? App\Models\Content::find($old) : null;
-                      $newContent = $new ? App\Models\Content::find($new) : null;
-                      // <span class="field">{{trans('admin.poem.columns.content_id')}}</span>&nbsp;[&nbsp;<del>{{$oldContent ? $oldContent->content : ''}}</del>&nbsp;⟹&nbsp;{{$newContent ? $newContent->content : ''}}&nbsp;]
-                    @endphp
-                    <span class="field">{{trans('admin.poem.columns.content_id')}}</span>&nbsp;[&nbsp;<del>{{$old}}</del>&nbsp;⟹&nbsp;{{$new}}&nbsp;]
-                @elseif($prop === 'is_original')
-                  <span class="field">{{trans('admin.poem.columns.is_original')}}</span>&nbsp;[&nbsp;<del>{{trans_choice('admin.poem.is_original', $old)}}</del>&nbsp;⟹&nbsp;{{trans_choice('admin.poem.is_original', $new)}}&nbsp;]
-                @elseif($prop === 'language_id')
-                  <span class="field">{{trans('admin.poem.columns.language_id')}}</span>&nbsp;[&nbsp;<del>{{$old ? $languageList[$old]->name_lang : ''}}</del>&nbsp;⟹&nbsp;{{$new ? $languageList[$new]->name_lang : ''}}&nbsp;]
-                @elseif($prop === 'genre_id')
-                  <span class="field">{{trans('admin.poem.columns.genre_id')}}</span>&nbsp;[&nbsp;<del>{{$old ? $genreList[$old]->name_lang : ''}}</del>&nbsp;⟹&nbsp;{{$new ? $genreList[$new]->name_lang : ''}}&nbsp;]
-                @elseif($prop === 'original_id')
-                    @php
-                      $oldPoem = $old ? App\Models\Poem::find($old) : null;
-                      $newPoem = $new ? App\Models\Poem::find($new) : null;
-                      $oldLink = $oldPoem ? "<a href=\"$oldPoem->url\">$oldPoem->title</a>" : '';
-                      $newLink = $newPoem ? "<a href=\"$newPoem->url\">$newPoem->title</a>" : '';
-                    @endphp
-                    <span class="field">{{trans('admin.poem.columns.original_id')}}</span>&nbsp;[&nbsp;<del>{!!$oldLink!!}</del>&nbsp;⟹&nbsp;{!!$newLink!!}&nbsp;]
-                @else
-                    <span class="field">{{trans('admin.poem.columns.'.$prop)}}</span>&nbsp;[&nbsp;<del>{{$old}}</del>&nbsp;⟹&nbsp;{{$new}}&nbsp;]
-                @endif
-            </div>
             @endforeach
         @elseif($log->description === 'created')
             @lang('poem.initial version')
@@ -201,9 +92,11 @@ use Jfcherng\Diff\Factory\RendererFactory;
       margin-right: .5em;
     }
     .log del {
-      white-space: pre;
       background-color: lightpink;
       text-decoration: none;
+    }
+    .log.poem del{
+      white-space: pre;
     }
     .change .new>ins {
       background-color: rgb(190, 230, 190);
