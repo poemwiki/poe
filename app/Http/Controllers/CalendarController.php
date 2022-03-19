1a1af3c82a2344853611d248780f2e14d1b22c68
Ray Victor <ray7551@gmail.com>
2022-03-20 00:33:30 +0800

    format

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -2,26 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Console\Commands\SPARQLQueryDispatcher;
 use App\Models\WikidataPoetDate;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
 
 class CalendarController extends Controller {
-
     // public function __construct() {
     // }
     public function index() {
         return view('calendar.index');
     }
 
-
     public function query($month, $day, $offset = 0, $force = 0) {
         $key = "calendar-query-$month-$day-$offset";
-        if(!$force) {
+        if (!$force) {
             $cache = Cache::get($key);
-            if($cache) return $cache;
+            if ($cache) {
+                return $cache;
+            }
         }
 
         $json = $this->response([
@@ -29,6 +26,7 @@ class CalendarController extends Controller {
             'death' => $this->queryDate($month, $day, 'death', $offset = 0)
         ]);
         Cache::put($key, $json, now()->addMinutes(60 * 24 * 30));
+
         return $json;
     }
 
@@ -42,13 +40,13 @@ class CalendarController extends Controller {
      * WHERE JSON_EXTRACT(`data`, "$.claims.P569") like "%11-09T%" limit 10
      */
     public function queryDate($month, $day, $type, $offset = 0) {
-        $yearField = $type."_year";
-        $monthField = $type."_month";
-        $dayField = $type."_day";
-        $precisionField = $type."_time_precision";
-        $data = WikidataPoetDate::where([
-            $monthField => sprintf('%02d', $month),
-            $dayField => sprintf('%02d', $day),
+        $yearField      = $type . '_year';
+        $monthField     = $type . '_month';
+        $dayField       = $type . '_day';
+        $precisionField = $type . '_time_precision';
+        $data           = WikidataPoetDate::where([
+            $monthField     => sprintf('%02d', $month),
+            $dayField       => sprintf('%02d', $day),
             $precisionField => 11
         ])
             ->orderBy($yearField, 'DESC')
