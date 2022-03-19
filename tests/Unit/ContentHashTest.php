1a823c3e8b485ddac50bf33185dd9ebc9d461471
Ray Victor <ray7551@gmail.com>
2022-03-19 21:37:20 +0800

    add test

--- tests/Unit/ContentHashTest.php
+++ tests/Unit/ContentHashTest.php
@@ -2,24 +2,21 @@
 
 namespace Tests\Unit;
 
-use App\Models\Poem;
-use PHPUnit\Framework\TestCase;
 use Illuminate\Support\Str;
+use PHPUnit\Framework\TestCase;
 
-class ContentHashTest extends TestCase
-{
+class ContentHashTest extends TestCase {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testNoSpaceTest()
-    {
+    public function testNoSpaceTest() {
         $this->assertEquals('', Str::noSpace(''));
         $this->assertEquals('', Str::noSpace(' '));
         $this->assertEquals('', Str::noSpace('  '));
         $this->assertEquals('ssf', Str::noSpace(" s\n
         sf"));
-        $this->assertEquals('ssf', Str::noSpace(" s　　sf "));
+        $this->assertEquals('ssf', Str::noSpace(' s　　sf '));
     }
 }
