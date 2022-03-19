6fd09a61f2e66ce01b15540ab88e31fe081b188f
Ray Victor <ray7551@gmail.com>
2022-03-20 00:37:39 +0800

    add transaction.f_id and transaction.memo

--- database/migrations/2022_03_15_205518_add_memo_to_transaction.php
+++ database/migrations/2022_03_15_205518_add_memo_to_transaction.php
@@ -4,16 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFIdToTransaction extends Migration {
+class AddMemoToTransaction extends Migration {
     public function up() {
         Schema::table('transaction', function (Blueprint $table) {
-            $table->unsignedBigInteger('f_id')->default(0)->nullable(false)->comment('parent transaction');
+            $table->string('memo')->nullable(true)->comment('can be price for listing transaction.');
         });
     }
 
     public function down() {
         Schema::table('transaction', function (Blueprint $table) {
-            $table->dropColumn('f_id');
+            $table->dropColumn('memo');
         });
     }
 }
