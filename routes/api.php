c593a35fd77d34336afcc572f4e4be49842bf553
Ray Victor <ray7551@gmail.com>
2022-04-05 02:42:18 +0800

    compatible with old weapp version

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware(['api'])->group(static function () {
 
             Route::get('/detail/{id}', '\App\Http\Controllers\API\PoemAPIController@detail')->name('detail');
             Route::get('/nft-detail/{id}', '\App\Http\Controllers\API\PoemAPIController@nftDetail')->name('detail');
-            Route::get('/share/{id}/{compositionID}', '\App\Http\Controllers\API\PoemAPIController@share')->name('share');
+            Route::get('/share/{id}/{compositionID?}', '\App\Http\Controllers\API\PoemAPIController@share')->name('share');
             Route::post('/q', [\App\Http\Controllers\API\PoemAPIController::class, 'query'])->name('query');
         });
         Route::prefix('user')->name('user/')->group(static function () {
