c883e30a0ff13789ebafb58a47df82e69bd08298
Ray Victor <ray7551@gmail.com>
2023-03-29 13:38:24 +0800

    get the newest proposal title from each proposal document

--- resources/views/page/proposals.blade.php
+++ resources/views/page/proposals.blade.php
@@ -1,7 +1,6 @@
 @php
-
 $token = Cache::remember('lark-tenant-access-token', 110 * 60, function () {
-  // get lark api token
+  // get lark api access token
   $res = Http::post('https://open.larksuite.com/open-apis/auth/v3/tenant_access_token/internal', [
       'app_id' => config('app.lark.app_id'),
       'app_secret' => config('app.lark.app_secret')
@@ -13,24 +12,40 @@ $token = Cache::remember('lark-tenant-access-token', 110 * 60, function () {
   return $res['tenant_access_token'];
 });
 
-$doc = Cache::remember('lark-docx', 15 * 60, function () use ($token) {
+$http = Http::withHeaders([
+    'Authorization' => 'Bearer '.$token
+]);
+$docTTL = 5 * 60;
+$docID = 'JvqZduINZoPudcxqmtuu07uXsQg';
+$doc = Cache::remember('lark-doc', $docTTL, function () use ($http, $token, $docTTL, $docID) {
   // get docx blocks form lark api
-  $doc = Http::withHeaders([
-      'Authorization' => 'Bearer '.$token
-  ])->get('https://open.larksuite.com/open-apis/docx/v1/documents/JvqZduINZoPudcxqmtuu07uXsQg/blocks?document_revision_id=-1&page_size=500')->json();
-
-  if($doc['code'] !== 0){
-      exit;
-  }
-
-  return $doc;
+  $url = 'https://open.larksuite.com/open-apis/docx/v1/documents/'.$docID.'/blocks?document_revision_id=-1&page_size=500';
+  return $http->get($url)->json();
 });
+
+if($doc['code'] !== 0){
+    exit;
+}
+
+foreach ($doc['data']['items'] as $key => &$item) {
+    if($item['block_type'] == 2 && isset($item['text']['elements'][0]['mention_doc'])){
+        $docToken = $item['text']['elements'][0]['mention_doc']['token'];
+        $proposalUrl = 'https://open.larksuite.com/open-apis/docx/v1/documents/' . $docToken;
+        $proposal = Cache::remember('lark-doc-' . $docToken, $docTTL * ($key + 1), function() use ($http, $proposalUrl) {
+            return $http->get($proposalUrl)->json();
+        });
+
+        if($proposal['code'] === 0){
+            $item['text']['elements'][0]['mention_doc']['real_title'] = $proposal['data']['document']['title'];
+        }
+    }
+}
 
 $titleBlock = $doc['data']['items'][0];
 $blocks = $doc['data']['items'];
 @endphp
 
-@extends(App\User::isWechat() ? 'layouts.webview' : 'layouts.fe')
+@extends(App\User::isWeAppWebview() ? 'layouts.webview' : 'layouts.fe')
 
 @section('title')提案与积分@endsection
 @section('author')
@@ -39,11 +54,11 @@ $blocks = $doc['data']['items'];
 
 @section('content')
   <h1>{{$titleBlock['page']['elements'][0]['text_run']['content']}}</h1>
-  @foreach($blocks as $item)
-    @if($item['block_type'] == 2)
-      @foreach($item['text']['elements'] as $element)
+  @foreach($blocks as $block)
+    @if($block['block_type'] == 2)
+      @foreach($block['text']['elements'] as $element)
         @if(isset($element['mention_doc']))
-          <p><a href="{{$element['mention_doc']['url']}}" target="_blank">{{$element['mention_doc']['title']}}</p>
+          <p><a href="{{$element['mention_doc']['url']}}" target="_blank">{{$element['mention_doc']['real_title'] ?? $element['mention_doc']['title']}}</p>
         @endif
       @endforeach
     @endif
