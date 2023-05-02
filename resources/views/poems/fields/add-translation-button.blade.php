1d521c624852ff332d99825f7d67db11c144191b
Ray Victor <ray7551@gmail.com>
2023-05-03 00:50:06 +0800

    btn text size

--- resources/views/poems/fields/add-translation-button.blade.php
+++ resources/views/poems/fields/add-translation-button.blade.php
@@ -3,10 +3,10 @@ $createPageUrl = route('poems/create', ['original_fake_id' => $poem->topOriginal
 ?>
 @if($poem->is_translated && !$poem->originalPoem)
   <dt>@lang('poem.no original work related')</dt>
-  <dd><a class="btn" href="{{ Auth::check() ? route('poems/create', ['translated_fake_id' => $poem->fake_id]) : route('login', ['ref' => route('poems/create', ['translated_fake_id' => $poem->fake_id], false)]) }}">
+  <dd><a class="text-sm btn" href="{{ Auth::check() ? route('poems/create', ['translated_fake_id' => $poem->fake_id]) : route('login', ['ref' => route('poems/create', ['translated_fake_id' => $poem->fake_id], false)]) }}">
       @lang('poem.add original work')</a></dd><br>
 @else
-  <dt><a class="btn create add-translate-btn"
+  <dt><a class="text-xs btn create add-translate-btn"
          href="{{ Auth::check() ? $createPageUrl : route('login', ['ref' => $createPageUrl]) }}">
       @lang('poem.add another translated version')
     </a>
