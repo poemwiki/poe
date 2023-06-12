fcb89873892f1562c2a948b1901d1b3803c817a6
Ray Victor <ray7551@gmail.com>
2023-06-13 07:08:44 +0800

    remove api route prefix "api."

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -80,7 +80,6 @@ class RouteServiceProvider extends ServiceProvider {
     protected function mapApiRoutes() {
         Route::prefix('api')
             ->middleware('api')
-            ->as('api.')
             ->namespace($this->namespace . '\\API')
             ->group(base_path('routes/api.php'));
     }
