8b7de24889c95beef9e0a61a5cbe40b03ae0d43d
star8ks <ray7551@gmail.com>
2025-07-28 15:07:35 +0800

    fix(database): enable persistent connection and buffered queries for MySQL

--- config/database.php
+++ config/database.php
@@ -62,6 +62,8 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::ATTR_TIMEOUT => 20,
+                PDO::ATTR_PERSISTENT => true,
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
             ]) : [],
         ],
 
