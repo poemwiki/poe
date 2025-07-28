42af12cbb27557fe6114b37576d4e45dd13fc75d
vico <ray7551@gmail.com>
2025-07-28 10:18:03 +0800

    fix(database): enable persistent connections for MySQL

--- config/database.php
+++ config/database.php
@@ -60,6 +60,7 @@ return [
             'options'        => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::ATTR_TIMEOUT      => 20,
+                PDO::ATTR_PERSISTENT => true,
             ]) : [],
         ],
 
