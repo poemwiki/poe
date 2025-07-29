b12fd31a6e79088b4069b48b2c508aaf92049d4a
vico <ray7551@gmail.com>
2025-07-30 00:21:22 +0800

    fix laravel mix build error

--- resources/js/admin/bootstrap.js
+++ resources/js/admin/bootstrap.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import _ from 'lodash';
 import Vue from 'vue';
-import moment from 'moment';
 
 window.Vue = Vue;
 window._ = _;
 window.axios = axios;
-window.moment = moment;
 
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
