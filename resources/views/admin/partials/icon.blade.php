e0abef0c9d24ebee459b3dc3835107b61df0de13
vico <ray7551@gmail.com>
2025-08-20 07:35:29 +0800

    fix(icon): update favicon link to use new SVG asset and remove unnecessary meta tags

--- resources/views/admin/partials/icon.blade.php
+++ resources/views/admin/partials/icon.blade.php
@@ -1,3 +1,1 @@
-<link rel="icon" type="image/svg" sizes="32x32" href="{{cosUrl('/img/common/poemwiki.svg')}}">
-<meta name="msapplication-TileColor" content="#5251b7">
-<meta name="theme-color" content="#5251b7">
+<link rel="icon" type="image/svg+xml" sizes="32x32" href="{{cosUrl('/img/common/favicon.svg')}}">
